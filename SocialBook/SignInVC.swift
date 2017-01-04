2f0d6e7d88b173269b17b209b4577427dfcad440
akilkumar28 <akil.psn@gmail.com>
2017-01-04 19:33:34 +0530

    Firebase and Project setup
    downloaded only the firebase pods and the project setup is complete .

---
+++ SocialBook/SignInVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SocialBook
+//
+//  Created by AKIL KUMAR THOTA on 1/4/17.
+//  Copyright © 2017 AKIL KUMAR THOTA. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+
+
+}
+
